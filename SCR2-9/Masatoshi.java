e451559863ca8facdb2d3decf21e092740485fe9
mushmoto <14163@hakodate.kosen-ac.jp>
2018-11-22 11:22:47 +0900

    Masathosiにup処理を追加

--- SCR2-9/Masatoshi.java
+++ SCR2-9/Masatoshi.java
@@ -14,6 +14,9 @@ public class Masatoshi extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "up" ) ){
+            setRotation(270);
+            move(2);
+        }
     }    
 }
