140a234ec1efc3c349e3ffe34259d9a21300d24f
nao0803 <14154@hakodate.kosen-ac.jp>
2018-11-22 11:22:39 +0900

    左

--- SCR2-9/Masatoshi.java
+++ SCR2-9/Masatoshi.java
@@ -15,5 +15,10 @@ public class Masatoshi extends Actor
     public void act() 
     {
         // Add your action code here.
+        if( Greenfoot.isKeyDown( "left" ) ){
+            setRotation(180);
+            move(2);
+        }
+
     }    
 }
